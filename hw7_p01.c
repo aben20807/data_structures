39085ee22ac248288140e1ccaff335f25e3c35b3
aben20807 <aben20807@gmail.com>
2017-01-13 18:30:51 +0800

    hw7 link can be debug

--- hw7_p01.c
+++ hw7_p01.c
@@ -2,20 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MALLOC(p, s) \
+		if(!((p) = malloc(s))){ \
+			fprintf(stderr, "Insufficient memory"); \
+			exit(EXIT_FAILURE); \
+		}
 #define FILE_NAME "Sample_input.txt"
 
 typedef struct node *nodePtr;
 typedef struct node{
     int vertex;
+    int dur;
     nodePtr link;
 } node;
 typedef struct{
     int count;
     nodePtr link;
-} headNode;
+} *headNode;
 
 void openFile(FILE **, const char *);//open file
-
 nodePtr readData(FILE *, headNode [], const int);
 
 int main()
@@ -26,10 +31,14 @@ int main()
     fscanf(fin, "%d", &numOfVertex);
     headNode hNode[numOfVertex];
     readData(fin, hNode, numOfVertex);
+    fclose(fin);
 
-    
-    for(int i = 0; i < numOfVertex; i++){
-        printf("%d\n", hNode[i].count);
+    int i;
+    for(i = 0; i < numOfVertex; i++){
+        printf("%d : %d(count) ", i, hNode[i]->count);
+        if(hNode[i]->link != NULL){
+            printf("%d(dur1)\n", hNode[i]->link->dur);
+        }
     }
     return 0;
 }
@@ -49,28 +58,34 @@ void openFile(FILE **fin, const char *fileName){
 nodePtr readData(FILE *fin, headNode hNode[], const int numOfVertex){
     int i, j;
     for(i = 0; i < numOfVertex; i++){
-        hNode[i].count = 0;
-        hNode[i].link = NULL;
+        MALLOC(hNode[i], sizeof(*hNode[i]));
+    }
+    for(i = 0; i < numOfVertex; i++){
+        hNode[i]->count = 0;
+        hNode[i]->link = NULL;
     }
     int tmpValue;
     headNode tmpHead;
-    node tmpCurrent;
+    nodePtr tmpCurrent;
+    MALLOC(tmpCurrent, sizeof(*tmpCurrent));
     for(i = 0; i < numOfVertex; i++){
         tmpHead = hNode[i];
         for(j = 0; j < numOfVertex; j++){
             fscanf(fin, "%d", &tmpValue);
             if(tmpValue){
-                printf("in (%d, %d)\n", i, j);
-                node tmpNode;
-                tmpNode.vertex = j;
-                tmpNode.link = NULL;
-                if(tmpHead.count == 0){
-                    tmpHead.link = &tmpNode;
+                // printf("(%d, %d)has dur\n", i, j);
+                nodePtr tmpNode;
+                MALLOC(tmpNode, sizeof(*tmpNode));
+                tmpNode->vertex = j;
+                tmpNode->dur = tmpValue;
+                tmpNode->link = NULL;
+                if(tmpHead->count == 0){
+                    tmpHead->link = tmpNode;
                 }
                 else{
-                    tmpCurrent.link = &tmpNode;
+                    tmpCurrent->link = tmpNode;
                 }
-                hNode[i].count ++;
+                hNode[i]->count ++;
                 tmpCurrent = tmpNode;
             }
         }
